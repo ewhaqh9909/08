ccfa34cf275f31c5b201402df685dcdac1b4a0cf
ewhaqh9909 <qhdud0921@naver.com>
2018-11-22 15:24:12 +0900

    2

--- 08_1.c
+++ 08_1.c
@@ -4,19 +4,21 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char src[100]="programing course";
-	char dst[100];
+	char src[100]="programming course";
+	//char dst[100];
 	int i=0;
 	
 	while(src[i]!='\0')
 	{
-		dst[i]=src[i];
+		//dst[i]=src[i];
 		i++;
 	}
-	dst[i]='\0';
 	
-	printf("%s\n",dst); 
+	//dst[i]='\0';
+	printf("%s(%d)\n",src,i); 
+	printf("%i",strlen(src));
 	
+	//strcpy(dst,src);
 	
 	return 0;
 }
